8c9907ba5bba6ed250b901d22c6311dda08be4f2
jianglongjian <2511067577@qq.com>
2018-07-23 10:57:27 +0800

    Initial Commit

---
+++ JXScanDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXScanDemo
+//
+//  Created by zl on 2018/7/23.
+//  Copyright © 2018年 longjianjiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
